dc3da5c453d37f5740169bbfb89f8222d4bb1aae
chemonoworld <jinwoo0451@naver.com>
2022-10-30 04:03:50 +0900

    feat: set up mainpage layout

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,9 +1,29 @@
 import "./mainpage.scss";
+import Button from '../../components/Button';
 
 const MainPage = () => {
+    const handleClick = () => {
+
+    }
     return(
         <div className="root-container">
-            Main page
+            <div className="main-expl-body">
+                <div>
+                    빗썸 아카데미 NFT를 민팅하고 트위터 아이디와 연결하여
+                </div>
+                <div>
+                    크롬 익스텐션을 통해 BTA 수료 뱃지를 확인해보세요!
+                </div>
+            </div>
+            
+            <div className="btn-box">
+                <Button onClick={handleClick} className="main-btn mint-btn">
+                    <div className="main-btn-txt">Mint BTA NFT</div>
+                </Button>
+                <Button onClick={handleClick} className="main-btn extension-btn btn-white">
+                    <div className="main-btn-txt">Download Extension</div>
+                </Button>
+            </div>
         </div>
     )
 }
